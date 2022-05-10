db7549395e122c1b0aaae4f9224b67f0bc7583e4
Andrinha <andrinha.eru@gmail.com>
2022-05-10 17:25:54 +0400

    add second page beta 0.7

--- frontend/src/components/Footer/SecondFooter.jsx
+++ frontend/src/components/Footer/SecondFooter.jsx
@@ -5,7 +5,9 @@ import './Footer.css';
 const SecondFooter = () => {
     return (
         <div className="footer2">
-            <div className="github2"/>
+            <div className="github2">
+                <img src="https://avatars.githubusercontent.com/u/9919?s=200&v=4"/>
+            </div>
         </div>
     );
 };
