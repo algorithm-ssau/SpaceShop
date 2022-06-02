30cac518ae99202d722121e675b7d1173e88a713
polinager <polina-gerasimova16@rambler.ru>
2022-06-02 20:33:50 +0500

    Add the top of the page About

---
+++ frontend/src/components/About/Greatings.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './About.css'
+
+const Greatings = () => {
+
+    return(
+        <div>
+            <div className="pict"></div>
+            <div className="text5">Рады приветствовать вас на сайте</div>
+            <div className="text6">SpaceShop</div>
+            <div className="moon"></div>
+        </div>
+    );
+}
+export default Greatings;
