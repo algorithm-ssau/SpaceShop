7438b9ef5617013beceb36130729e2a74f68a01f
polinager <polina-gerasimova16@rambler.ru>
2022-06-02 20:54:34 +0500

    Add the top of the page Help

---
+++ frontend/src/components/Help/HelpHeader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Help.css'
+
+const HelpHeader = () => {
+    return(
+        <div>
+            <div className="text17">5 самых часто задаваемых вопросов</div>
+            <div className="text18">SpaceShop</div>
+        </div>
+    );
+}
+export default HelpHeader;
