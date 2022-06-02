07378980845e1530aae6c393d974125abb8f1eaf
polinager <polina-gerasimova16@rambler.ru>
2022-06-02 20:36:21 +0500

    Add the quote of JS in the page About

---
+++ frontend/src/components/About/InBorder.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './About.css'
+
+const InBorder = () => {
+
+    return(
+        <div>
+            <div className="text7">Джейсон Майкл Стэтхем</div>
+            <div className="text8">С 2022 года помогаем нашим клиентам делать выгодные покупки космической продукции</div>
+            <div className="text9">Основатель и генеральный директор компании SpaceShop</div>
+            <div className="text10"></div>
+        </div>
+    );
+}
+export default InBorder;
